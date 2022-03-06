60d1a07c36b01972a58049be5b58342d42ea208a
Nickyw <nicky.w@binance.com>
2022-03-07 00:11:07 +0800

    test_api

---
+++ app/src/main/java/com/alibaba/android/arouter/demo/kotlin/KotlinTestActivity.kt
@@ -0,0 +1,24 @@
+package com.alibaba.android.arouter.demo.kotlin
+
+import android.app.Activity
+import android.os.Bundle
+import com.alibaba.android.arouter.demo.R
+import com.alibaba.android.arouter.facade.annotation.Autowired
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.android.arouter.launcher.ARouter
+
+@Route(path = "/kotlin/test")
+class KotlinTestActivity : Activity() {
+
+    @Autowired
+    @JvmField var name: String? = null
+    @Autowired
+    @JvmField var age: Int? = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        ARouter.getInstance().inject(this)  // Start auto inject.
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_test)
+    }
+}
---
+++ app/src/main/java/com/alibaba/android/arouter/demo/module1/TestModuleActivity.java
@@ -0,0 +1,17 @@
+package com.alibaba.android.arouter.demo.module1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.alibaba.android.arouter.demo.R;
+import com.alibaba.android.arouter.facade.annotation.Route;
+
+@Route(path = "/module/1")
+public class TestModuleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_module);
+    }
+}
